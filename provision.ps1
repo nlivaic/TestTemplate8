a01cd16da747cc33018d2ee73b109dc54b8b1286
nlivaic <nenad.livaic@gmail.com>
2024-01-01 21:10:49 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -5,6 +5,11 @@
 # Resource group
 $jsonResultRg = az deployment sub create --location $env:LOCATION --template-file ./deployment/resource-group.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME location=$env:LOCATION | ConvertFrom-Json
 $resourceGroupName = $jsonResultRg.properties.outputs.resourceGroupName.value
+Write-Host "##vso[task.setvariable variable=resourceGroupName;isoutput=true]$resourceGroupName"
 
 # Resources
-az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$env:DB_PASSWORD authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer'
+$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$env:DB_PASSWORD authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' | ConvertFrom-Json
+$appServiceWebName = $jsonResultAll.properties.outputs.appServiceWebName.value
+Write-Host "##vso[task.setvariable variable=appServiceWebName;isoutput=true]$appServiceWebName"
+$dbConnection = $jsonResultAll.properties.outputs.dbConnection.value
+Write-Host "##vso[task.setvariable variable=dbConnection;isoutput=true]$dbConnection"
