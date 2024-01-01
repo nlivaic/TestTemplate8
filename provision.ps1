200373109b2ed13b21885aa6c7ec9b23db55fb50
nlivaic <nenad.livaic@gmail.com>
2024-01-01 16:47:41 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -7,4 +7,4 @@ $jsonResultRg = az deployment sub create --location $env:LOCATION --template-fil
 $resourceGroupName = $jsonResultRg.properties.outputs.resourceGroupName.value
 
 # Resources
-az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$env:DB_PASSWORD
+az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$env:DB_PASSWORD authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer'
