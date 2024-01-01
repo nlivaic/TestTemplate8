eb2742ed5f803dd30fcf9b01ac1550c0dd3c16a5
nlivaic <nenad.livaic@gmail.com>
2024-01-01 13:55:13 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,17 +1,10 @@
 # Use this file only to provision Azure resources from local.
 
 #az account set -s $SUBSCRIPTION
-echo '::::::::::::::::::::::::::::::::::::::::env vars START::::::::::::::::::::::::::::::::::::::::'
-echo $env:LOCATION
-echo $env:ENVIRONMENT
-echo $env:PROJECT_NAME
-echo $env:DB_USER
-echo $env:DB_PASSWORD
-echo '::::::::::::::::::::::::::::::::::::::::env vars END::::::::::::::::::::::::::::::::::::::::'
 
 # Resource group
-$jsonResultRg = az deployment sub create --location $LOCATION --template-file ./deployment/resource-group.bicep --parameters environment=$ENVIRONMENT projectName=$PROJECT_NAME location=$LOCATION | ConvertFrom-Json
+$jsonResultRg = az deployment sub create --location $env:LOCATION --template-file ./deployment/resource-group.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME location=$env:LOCATION | ConvertFrom-Json
 $resourceGroupName = $jsonResultRg.properties.outputs.resourceGroupName.value
 
 # Resources
-az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$ENVIRONMENT projectName=$PROJECT_NAME db_user=$DB_USER db_password=$DB_PASSWORD
+az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$env:DB_PASSWORD
