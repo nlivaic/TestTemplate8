acc6b4ea8319ffbb944f83e2ec5dd70e674d97d5
nlivaic <nenad.livaic@gmail.com>
2024-01-03 10:17:19 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,8 +1,9 @@
 # Use this file only to provision Azure resources from local.
 
 #az account set -s $SUBSCRIPTION
+$dbPassword = $args[0]
 echo "=============================================================================================================================="
-echo $(DB_PASSWORD)
+echo $dbPassword
 echo "=============================================================================================================================="
 # Resource group
 $jsonResultRg = az deployment sub create --location $env:LOCATION --template-file ./deployment/resource-group.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME location=$env:LOCATION | ConvertFrom-Json
@@ -10,7 +11,7 @@ $resourceGroupName = $jsonResultRg.properties.outputs.resourceGroupName.value
 Write-Host "##vso[task.setvariable variable=resourceGroupName;isoutput=true]$resourceGroupName"
 
 # Resources
-$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$env:DB_PASSWORD authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' --debug | ConvertFrom-Json
+$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$dbPassword authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' --debug | ConvertFrom-Json
 $appServiceWebName = $jsonResultAll.properties.outputs.appServiceWebName.value
 Write-Host "##vso[task.setvariable variable=appServiceWebName;isoutput=true]$appServiceWebName"
 $dbConnection = $jsonResultAll.properties.outputs.dbConnection.value
