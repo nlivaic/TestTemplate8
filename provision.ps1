af28c19d06bcc7e3a415951dae737a805219141f
nlivaic <nenad.livaic@gmail.com>
2024-01-03 22:59:10 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,17 +1,17 @@
 # Use this file only to provision Azure resources from local.
-param ([SecureString] $dbPassword)
+param ($location, $environment, $projectName, $dbUser, $dbPassword)
 #az account set -s $SUBSCRIPTION
 echo "=============================================================================================================================="
 echo $dbPassword
 echo "=============================================================================================================================="
 
 # Resource group
-$jsonResultRg = az deployment sub create --location $env:LOCATION --template-file ./deployment/resource-group.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME location=$env:LOCATION | ConvertFrom-Json
+$jsonResultRg = az deployment sub create --location $location --template-file ./deployment/resource-group.bicep --parameters environment=$environment projectName=$projectName location=$location | ConvertFrom-Json
 $resourceGroupName = $jsonResultRg.properties.outputs.resourceGroupName.value
 Write-Host "##vso[task.setvariable variable=resourceGroupName;isoutput=true]$resourceGroupName"
 
 # Resources
-$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$env:ENVIRONMENT projectName=$env:PROJECT_NAME db_user=$env:DB_USER db_password=$dbPassword authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' --debug | ConvertFrom-Json
+$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$environment projectName=$projectName db_user=$dbUser db_password=$dbPassword authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' --debug | ConvertFrom-Json
 $appServiceWebName = $jsonResultAll.properties.outputs.appServiceWebName.value
 Write-Host "##vso[task.setvariable variable=appServiceWebName;isoutput=true]$appServiceWebName"
 $dbConnection = $jsonResultAll.properties.outputs.dbConnection.value
