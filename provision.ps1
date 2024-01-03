a90ae6f9f6dfd93995ad677e2ed799ffe53c10b3
nlivaic <nenad.livaic@gmail.com>
2024-01-03 23:43:20 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,9 +1,7 @@
 # Use this file only to provision Azure resources from local.
 param ($location, $environment, $projectName, $dbUser, $dbPassword)
 #az account set -s $SUBSCRIPTION
-echo "=============================================================================================================================="
-echo $dbPassword
-echo "=============================================================================================================================="
+$entraApiConfiguration = .\deployment\entraApiRegistration.ps1 "$($projectName)api"
 
 # Resource group
 $jsonResultRg = az deployment sub create --location $location --template-file ./deployment/resource-group.bicep --parameters environment=$environment projectName=$projectName location=$location | ConvertFrom-Json
@@ -11,7 +9,7 @@ $resourceGroupName = $jsonResultRg.properties.outputs.resourceGroupName.value
 Write-Host "##vso[task.setvariable variable=resourceGroupName;isoutput=true]$resourceGroupName"
 
 # Resources
-$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$environment projectName=$projectName db_user=$dbUser db_password=$dbPassword authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' --debug | ConvertFrom-Json
+$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$environment projectName=$projectName db_user=$dbUser db_password=$dbPassword authAuthority=$entraApiConfiguration.AuthAuthority authAudience=$entraApiConfiguration.AuthAudience authValidIssuer=$entraApiConfiguration.AuthValidIssuer | ConvertFrom-Json
 $appServiceWebName = $jsonResultAll.properties.outputs.appServiceWebName.value
 Write-Host "##vso[task.setvariable variable=appServiceWebName;isoutput=true]$appServiceWebName"
 $dbConnection = $jsonResultAll.properties.outputs.dbConnection.value
