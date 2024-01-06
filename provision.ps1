bee27b3e3f98a9f3391a2ecfd6fe0e1abaaceaa6
nlivaic <nenad.livaic@gmail.com>
2024-01-06 16:49:20 +0100

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -14,7 +14,7 @@ Write-Host "------ Create Resource Group END ------"
 
 # Resources
 Write-Host "------ Create Resources START ------"
-$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$environment projectName=$projectName db_user=$dbUser db_password=$dbPassword authAuthority='$entraApiConfiguration.AuthAuthority' authAudience='$entraApiConfiguration.AuthAudience' authValidIssuer='$entraApiConfiguration.AuthValidIssuer' | ConvertFrom-Json
+$jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file .\deployment\iac.bicep --parameters environment=$environment projectName=$projectName db_user=$dbUser db_password=$dbPassword authAuthority=$entraApiConfiguration.AuthAuthority authAudience=$entraApiConfiguration.AuthAudience authValidIssuer=$entraApiConfiguration.AuthValidIssuer | ConvertFrom-Json
 $appServiceWebName = $jsonResultAll.properties.outputs.appServiceWebName.value
 Write-Host "##vso[task.setvariable variable=appServiceWebName;isoutput=true]$appServiceWebName"
 $dbConnection = $jsonResultAll.properties.outputs.dbConnection.value
