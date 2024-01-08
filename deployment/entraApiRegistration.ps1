72f0423f5386a9fca28cde3d817f3263c3a259f6
Nenad Livaic <nlivaic@alixpartners.com>
2024-01-08 22:44:04 +0100

    dev: Format resource provision output (with colours). Remove unused scripts. [+semver: minor] [+semver: minor]

--- deployment/entraApiRegistration.ps1
+++ deployment/entraApiRegistration.ps1
@@ -5,8 +5,8 @@ Param( [string]$projectName = "" )
 
 $displayNameApi = "$($projectName)Api"
 
-Write-Host "--- Create Azure App Registration - START ---"
-Write-Host "displayNameApi: $displayNameApi"
+Write-Host "--- Create Azure App Registration - START ---" -ForegroundColor Yellow
+Write-Host "displayNameApi: $displayNameApi" -ForegroundColor Green
 $appRegistration = az ad app create `
     --display-name $displayNameApi `
     --sign-in-audience AzureADMyOrg `
@@ -16,21 +16,21 @@ $appRegistrationResult = ($appRegistration | ConvertFrom-Json)
 $appRegistrationResultAppId = $appRegistrationResult.appId
 $azAppOID = (az ad app show --id $appRegistrationResultAppId  | ConvertFrom-JSON).id
 
-Write-Host "Created API $displayNameApi with appId: $appRegistrationResultAppId"
-Write-Host "--- Create Azure App Registration - END ---"
+Write-Host "Created API $displayNameApi with appId: $appRegistrationResultAppId" -ForegroundColor Green
+Write-Host "--- Create Azure App Registration - END ---" -ForegroundColor Yellow
 
 ##################################
 ### Expose API
 ##################################
-Write-Host "--- Expose API - START ---"
+Write-Host "--- Expose API - START ---" -ForegroundColor Yellow
 az ad app update --id $appRegistrationResultAppId --identifier-uris api://$appRegistrationResultAppId
-Write-Host "API $displayNameApi exposed"
-Write-Host "--- Expose API - END ---"
+Write-Host "API $displayNameApi exposed" -ForegroundColor Green
+Write-Host "--- Expose API - END ---" -ForegroundColor Yellow
 
 ##################################
 ###  Add scopes (oauth2Permissions)
 ##################################
-Write-Host "--- Add scopes - START ---"
+Write-Host "--- Add scopes - START ---" -ForegroundColor Yellow
 # 0. Setup some basic stuff to work with scopes.
 $headerJson = @{
     'Content-Type' = 'application/json'
@@ -39,7 +39,7 @@ $headerJson = $headerJson.replace('"', '\"')
 $graphURL="https://graph.microsoft.com/v1.0/applications/$azAppOID"
 
 Write-Host ""
-Write-Host "Disabling existing scopes..."
+Write-Host "Disabling existing scopes..." -ForegroundColor Green
 # 1. Read existing scopes.
 $oauth2PermissionScopesApiOld = $appRegistrationResult.api
 # 2. Disable scopes, because we want to provision from a list.
@@ -54,24 +54,25 @@ $bodyOauth2PermissionScopesApiOldJsonEscaped = ($bodyOauth2PermissionScopesApiOl
 $bodyOauth2PermissionScopesApiOldJsonEscaped | Out-File -FilePath .\oauth2PermissionScopesOld.json
 az rest --method PATCH --uri $graphurl --headers $headerJson --body '@oauth2PermissionScopesOld.json'
 Remove-Item .\oauth2PermissionScopesOld.json
-Write-Host "Existing scopes disabled successfully."
+Write-Host "Existing scopes disabled successfully." -ForegroundColor Green
 
 # 3. Add new scopes from file oauth2PermissionScopes.json.
 Write-Host ""
-Write-Host "Creating scopes..."
+Write-Host "Creating scopes..." -ForegroundColor Green
 az rest --method PATCH --uri $graphurl --headers $headerJson --body '@deployment/oauth2PermissionScopes.json'
 # 4. Re-enable previously disabled scopes.
 if ($? -eq $false) {
-    Write-Error "Error creating scopes."
-    Write-Error "Re-enabling original scopes."
+    Write-Error "Error creating scopes." -ForegroundColor Red
+    Write-Error "Re-enabling original scopes." -ForegroundColor Red
     foreach ($scope in $oauth2PermissionScopesApiOld.oauth2PermissionScopes) {
-        Write-Host "`tExisting scope $($scope.value) re-enabled."
+        Write-Host "`tExisting scope $($scope.value) re-enabled." -ForegroundColor Red
         $scope.isEnabled = 'true'
     }
     $bodyOauth2PermissionScopesApiOldJsonEscaped = ($bodyOauth2PermissionScopesApiOld|ConvertTo-Json -d 4 -Compress)
     $bodyOauth2PermissionScopesApiOldJsonEscaped | Out-File -FilePath .\oauth2PermissionScopesOld.json
     az rest --method PATCH --uri $graphurl --headers $headerJson --body '@oauth2PermissionScopesOld.json'
     Remove-Item .\oauth2PermissionScopesOld.json
+    Write-Host "--- Add scopes - END (Error) ---" -ForegroundColor Red
     Return
 }
 # 5. If all went well, print confirmation message and list of new scopes.
@@ -79,30 +80,30 @@ $appRegistration = az ad app show --id $appRegistrationResultAppId
 $appRegistrationResult = ($appRegistration | ConvertFrom-Json)
 $oauth2PermissionScopesApi = $appRegistrationResult.api
 foreach ($scope in $oauth2PermissionScopesApi.oauth2PermissionScopes) {
-    Write-Host "`tScope created: $($scope.value)"
+    Write-Host "`tScope created: $($scope.value)" -ForegroundColor Green
 }
-Write-Host "Scopes created successfully."
-Write-Host "--- Add scopes - END ---"
+Write-Host "Scopes created successfully." -ForegroundColor Green
+Write-Host "--- Add scopes - END ---" -ForegroundColor Yellow
 
 Write-Host ""
-Write-Host "Registered App details:"
-Write-Host $appRegistration
+Write-Host "Registered App details:" -ForegroundColor Green
+Write-Host $appRegistration -ForegroundColor Green
 
 ##################################
 ###  Create a ServicePrincipal for the API App Registration
 ##################################
-Write-Host "--- Create a ServicePrincipal - START ---"
+Write-Host "--- Create a ServicePrincipal - START ---" -ForegroundColor Yellow
 
 $createdSp = az ad sp show --id $appRegistrationResultAppId
 if ($? -eq $false) {
     $createdSp = az ad sp create --id $appRegistrationResultAppId
-    Write-Host "Created Service Principal for API App registration"
+    Write-Host "Created Service Principal for API App registration" -ForegroundColor Green
 } else {
-    Write-Host "Service Principal already exists, skipped creation."
+    Write-Host "Service Principal already exists, skipped creation." -ForegroundColor Green
 }
-Write-Host "Service principal details:"
-Write-Host $createdSp
-Write-Host "--- Create a ServicePrincipal - END ---"
+Write-Host "Service principal details:" -ForegroundColor Green
+Write-Host $createdSp -ForegroundColor Green
+Write-Host "--- Create a ServicePrincipal - END ---" -ForegroundColor Yellow
 
 ##################################
 ###  Return configuration
@@ -110,9 +111,3 @@ Write-Host "--- Create a ServicePrincipal - END ---"
 Write-Host "##vso[task.setvariable variable=authAuthority;isoutput=true]https://login.microsoftonline.com/$azAppOID/v2.0"
 Write-Host "##vso[task.setvariable variable=authAudience;isoutput=true]api://$appRegistrationResultAppId"
 Write-Host "##vso[task.setvariable variable=authValidIssuer;isoutput=true]https://sts.windows.net/$azAppOID/"
-
-#[hashtable]$Configuration = @{} 
-#$Configuration.AuthAuthority = [string]"https://login.microsoftonline.com/$azAppOID/v2.0"
-#$Configuration.AuthAudience = [string]"api://$appRegistrationResultAppId" 
-#$Configuration.AuthValidIssuer = [string]"https://sts.windows.net/$azAppOID/" 
-#return $Configuration
